7c4b2f191d5441834ccda8df25937920f834c95d
Will Garrett <willgarrett64@yahoo.com>
2021-10-31 17:21:50 +0000

    add first stylings for inputs and labels

--- src/components/sidebars/SignIn.js
+++ src/components/sidebars/SignIn.js
@@ -2,10 +2,14 @@ export default function SignIn() {
   return (
     <div>
       <h2>Sign in to your account to access <strong>full features</strong></h2>
-      <p>USERNAME</p>
-      <input/>
-      <p>PASSWORD</p>
-      <input/>
+      <div>
+        <label for="username-signin">USERNAME</label>
+        <input id="username-signin" />
+      </div>
+      <div>
+        <label for="password-signin">PASSWORD</label>
+        <input id="password-signin" />
+      </div>
       <button>SIGN IN</button>
       <button>SIGN UP</button>
       <p>CONTINUE AS GUEST</p>
--- src/components/sidebars/SignUp.js
+++ src/components/sidebars/SignUp.js
@@ -2,12 +2,18 @@ export default function SignUp() {
   return (
     <div>
       <h2>SIGN UP</h2>
-      <p>USERNAME</p>
-      <input/>
-      <p>PASSWORD</p>
-      <input/>
-      <p>VERIFY PASSWORD</p>
-      <input/>
+      <div>
+        <label for="username-signup">USERNAME</label>
+        <input id="username-signup" />
+      </div>
+      <div>
+        <label for="password-signup">PASSWORD</label>
+        <input id="password-signup" />
+      </div>
+      <div>
+        <label for="verify-password-signup">VERIFY PASSWORD</label>
+        <input id="verify-password-signup" />
+      </div>
       <button>SIGN UP</button>
     </div>
   )
