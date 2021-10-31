d35c167d9fb94e9d2765e1ef1dea302ff3c4a345
Will Garrett <willgarrett64@yahoo.com>
2021-10-31 18:18:14 +0000

    add mobile media query

--- src/components/sidebars/SignUp.js
+++ src/components/sidebars/SignUp.js
@@ -1,7 +1,7 @@
 export default function SignUp() {
   return (
     <div className="content">
-      <h2>Create an account to access <strong>full features</strong></h2>
+      <h2>Create a new account to access <strong>full features</strong></h2>
       <div className="input-label-pair">
         <label for="username-signup">USERNAME</label>
         <input id="username-signup" />
