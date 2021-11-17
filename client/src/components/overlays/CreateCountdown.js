68fb37dcb19cc1fd0126004d4883bf4ba9663dfb
Will Garrett <willgarrett64@yahoo.com>
2021-11-17 21:48:45 +0000

    minor refactor of CreateCountdown component

--- client/src/components/overlays/CreateCountdown.js
+++ client/src/components/overlays/CreateCountdown.js
@@ -2,7 +2,7 @@
 import closeIcon from '../../images/close-icon.svg'
 
 // redux
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addCountdown, deleteCountdown, editCountdown, setCountdownList } from '../../redux/features/countdownListSlice';
 import { setLiveCountdown } from '../../redux/features/liveCountdownSlice';
 
@@ -12,32 +12,34 @@ import { apiRequest } from '../../utils/apiRequests';
 export default function CreateCountdown({toggleOverlayHidden}) {
   const dispatch = useDispatch();
   
+  // form elements
+  const nameInput = document.getElementById('new-countdown-name')
+  const dateInput = document.getElementById('new-countdown-date')
+  const timeInput = document.getElementById('new-countdown-time')
+
   const resetInputs = () => {
-    document.getElementById('new-countdown-name').value = '';
-    document.getElementById('new-countdown-date').value = '';
-    document.getElementById('new-countdown-time').value = '';
+    nameInput.value = '';
+    dateInput.value = '';
+    timeInput.value = '';
   }
 
   const handleAddNewCountdown = async () => {
-    let name = document.getElementById('new-countdown-name').value;
-    let date = document.getElementById('new-countdown-date').value;
-    let time = document.getElementById('new-countdown-time').value;
+    let name = nameInput.value;
+    let date = dateInput.value;
+    let time = timeInput.value;
 
+    // NEED TO IMPROVE FORM VALIDATION
     if (!name || !date || !time) {
       alert('Please enter a name, date and time');
       return
     }
 
-    const newCountdownObject = {
-      name: name,
-      date: date,
-      time: time,
-    }
+    const newCountdownObject = {name, date, time};
 
-    const data = await apiRequest.createNewCountdown(newCountdownObject);
+    const newCountdownResponse = await apiRequest.createNewCountdown(newCountdownObject);
 
-    dispatch(addCountdown(data));
-    dispatch(setLiveCountdown(data));
+    dispatch(addCountdown(newCountdownResponse));
+    dispatch(setLiveCountdown(newCountdownResponse));
     toggleOverlayHidden();
     resetInputs();
   }
